31b2437ae1ce669c96e61d7203c8d23ad72b654d
Alexis <Alexis20007121@gmail.com>
2019-10-04 10:27:09 -0700

    added some scripts

---
+++ Assets/alexisScene/healthcount.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class healthcount : MonoBehaviour
+{
+
+    public int healthpool = 3;
+    public Image h1;
+    public Image h2;
+    public Image h3;
+    public bool sub;
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "MonsterTag")
+            {
+            if (sub == true)
+            {
+                healthpool -= 1;
+                sub = false;
+            }
+
+            Destroy(other.gameObject);
+            print("monster col");
+            }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sub = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(healthpool==2)
+        {
+            h3.enabled = false;
+            sub = true;
+        }
+        if (healthpool==1)
+        {
+            h2.enabled = false;
+            sub = true;
+        }
+
+        if (healthpool == 0)
+        {
+            h1.enabled = false;
+            sub = true;
+        }
+
+    }
+
+
+
+}
